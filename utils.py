6c94974a9f0bce77f56668496cf508e84bfa2d22
vignesh <selvasitpl@gmail.com>
2023-03-30 12:53:50 +0530

    minor changes

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 class ProgressTracker:
     def __init__(self, key, total):
         def callBack(t, c, i, p):
-            print(f"{key} :- total: {t} | completed: {c} | increment: {i} | percentage: {p}\r", end="")
+            print(f"{key} :\n\t total: {t} | completed: {c} | increment: {i} | percentage: {p}\r", end="")
             if (p == 100):
                 print()
 
@@ -65,3 +65,56 @@ class Optional:
         if not self.value:
             callBack()
         return self
+
+class Region:
+    def __init__(self, region_name):
+        self.region = {'region': region_name, 'directions': {}}
+
+    def _direction(self, direction, count):
+        self.region.get('directions')[direction] = count
+        return self
+
+    def central(self, count):
+        return self._direction('central', count)
+
+    def east(self, count):
+        return self._direction('east', count)
+
+    def west(self, count):
+        return self._direction('west', count)
+
+    def north(self, count):
+        return self._direction('north', count)
+
+    def south(self, count):
+        return self._direction('south', count)
+
+    def southeast(self, count):
+        return self._direction('southeast', count)
+
+    def southwest(self, count):
+        return self._direction('southwest', count)
+
+    def northwest(self, count):
+        return self._direction('northwest', count)
+
+    def northeast(self, count):
+        return self._direction('northeast', count)
+
+    def getRegionLabels(self):
+        return [f"{self.region.get('region')}-{direction}-{str(number)}" for direction, count in self.region.get('directions').items() for number in range(1, count+1)]
+
+class Regions:
+    def __init__(self):
+        self.regions = []
+
+    def add(self, region):
+        self.regions.append(region)
+        return self
+
+    def getRegionLabels(self):
+        return [regionLabel for region in self.regions for regionLabel in region.getRegionLabels()]
+
+regions = Regions().add(Region('us').east(2).west(2)).add(Region('ap').south(2).northeast(3).southeast(2)).add(Region('ca').central(1)).add(Region('eu').central(1).west(3).north(1)).add(Region('sa').east(1))
+def getRegionLabels():
+    return regions.getRegionLabels()
